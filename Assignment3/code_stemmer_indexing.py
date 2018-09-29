db35e3bb47cff4b123b5aecc7a917351544c7393
Emily Holt <emily.holt@dartmouth.edu>
2018-09-29 12:30:26 -0400

    finishing writeup 3

--- Assignment3/code_stemmer_indexing.py
+++ Assignment3/code_stemmer_indexing.py
@@ -1,4 +1,6 @@
 # Natural Language Toolkit: code_stemmer_indexing
+import nltk
+from nltk.corpus import wordnet as wn
 
 class IndexedText(object):
 
@@ -17,9 +19,20 @@ class IndexedText(object):
             ldisplay = '{:>{width}}'.format(lcontext[-width:], width=width)
             rdisplay = '{:{width}}'.format(rcontext[:width], width=width)
             print(ldisplay, rdisplay)
-
-        wn.synsets('dog')[0].offset
+            semantic_index = wn.synsets(i)[0].offset
 
     def _stem(self, word):
         return self._stemmer.stem(word).lower()
+
+def concordance_extended(word, stemmer, text, width=40):
+    key = stemmer.stem(word).lower()
+    wc = int(width/4) # words of context
+    nltk.Index((stemmer.stem(word).lower(), i) for (i, word) in enumerate(text))
+    for i in self._index[key]:
+        lcontext = ' '.join(text[i-wc:i])
+        rcontext = ' '.join(text[i:i+wc])
+        ldisplay = '{:>{width}}'.format(lcontext[-width:], width=width)
+        rdisplay = '{:{width}}'.format(rcontext[:width], width=width)
+        print(ldisplay, rdisplay)
+        semantic_index = wn.synsets(i)[0].offset    
 
--- Assignment3/unknown.py
+++ Assignment3/unknown.py
@@ -1,21 +1,34 @@
+import bs4
+from bs4 import BeautifulSoup
 from urllib import request, response
 import nltk
 import re
 import ssl
 import sys
 
+def stem(word):
+	for suffix in ['ing', 'ly', 'ed', 'ious', 'ies', 'ive', 'es', 's', 'ment']:
+		if word.endswith(suffix):
+			return word[:-len(suffix)]
+	return word
+
 def calc_unknown(url):
 	ssl._create_default_https_context = ssl._create_unverified_context
-	response = request.urlopen(url)
-	raw = response.read().decode('utf8')
-	tokens = re.findall(r'/\b[a-z]+\b/', raw)
-	print(tokens)
+	html = request.urlopen(url).read().decode('utf8')
+	raw = BeautifulSoup(html).get_text()
+	tokens = re.findall(r'\b[a-z]+\b', raw)
+	# token_stems = re.findall(r'^.*(?:ing|ly|ed|ious|ies|ive|es|s|ment)$', raw)
 	known_words = [w for w in nltk.corpus.words.words('en') if w.islower()]
 	unknown = list(set(tokens).difference(set(known_words)))
 
-	print("Unknown words: %s" % unknown)
+	token_stems = [stem(w) for w in tokens]
+	unknown_stems = list(set(token_stems).difference(set(known_words)))
+
+
+	print("Unknown word stems: %s" % unknown_stems)
 	print("Length of raw response: %d" % len(raw))
 	print("Number of unknown words in of raw response: %d" % len(set(unknown)))
+	print("Number of unknown word stems in of raw response: %d" % len(set(unknown_stems)))
 	
 
 if __name__ == '__main__':
